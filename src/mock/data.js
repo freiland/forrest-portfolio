916d05ba157e99d96bb8129a61cebf3dee62ba4a
Forrest Reiland <you@example.com>
2021-04-23 15:36:02 -0700

    add new project data

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,18 +10,18 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Forrest',
+  subtitle: "I'm a full stack developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'A lifelong learner, I was drawn to programming because creating inspired projects is what keeps me growing.',
+  paragraphTwo: 'A tenacious personality drives me to always improve- between projects and beyond.',
+  paragraphThree: 'Outside of programming I love music and hiking. I have played guitar for most of my life, especially spanish classical music as well as American and British blues music. I think of myself first and foremost a creative individual who enjoys the challenge of problem solving. ',
+  resume: 'https://docs.google.com/document/d/1qUuasOiuLa5Xf6Gj7oNg6RqVe8aoefaOMyb98H96HsY/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,29 +29,29 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Hygge real estate',
+    info: 'A webpage to find rental data around Portland, OR using the Realy Mole API (No API key).',
+    info2: 'Technologies: JavaScript, jQuery, Bootstrap',
+    url: 'https://focused-curran-9e1d82.netlify.app/',
+    repo: 'https://github.com/freiland/hygge-real-estate', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'projectOne.jpeg',
+    title: 'Nombolo app',
+    info: 'Nombolo is a Portland based startup, and I am only a small part of a great team of developers who are building this application. The Nombolo app allows people to ask questions to their communities and receive video responses from other users! On the mobile app, I have developed features such a the Featured Business section on the homescreen and reply functionality. ',
+    info2: 'Technologies: React Native, GrapgQL, DynamoDB',
+    url: 'https://nombolo.com',
+    repo: 'https://nombolo.com', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'projectTwo.jpg',
+    title: 'The Tap Room',
+    info: 'An online brew house. A user can add and delete kegs to the page, or pour themself a pint! ',
+    info2: 'Technologies: React, JavaScript',
+    url: 'https://thetaproom.netlify.app',
+    repo: 'https://github.com/freiland/the-tap-room', // if no repo, the button will not show up
   },
 ];
 
@@ -59,31 +59,22 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'freiland@email.arizona.edu',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+   
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/forrestreiland',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/freiland',
     },
   ],
 };
