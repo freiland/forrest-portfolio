2386b1c5c027a943dfe3c657588ad4f51e0cc7e2
Forrest Reiland <you@example.com>
2021-04-26 21:06:07 -0700

    stash my changes to try and revert back to a previous commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
 export default () => {
-  const { title, lang, description } = headData;
+  const { lang, description } = headData;
+  const title = 'Forrest Reiland';
 
   return (
     <>
