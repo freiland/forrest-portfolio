daed6a1627a1b4952a7f00d614c117b9ef9c4e41
Forrest Reiland <you@example.com>
2021-04-28 15:04:29 -0700

    fix hero again

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,9 +27,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {'Hi, my name is'}{' '}
-            <span className="text-color-main">{name}</span>
+            <span className="text-color-main">{'Forrest'}</span>
             <br />
-            {subtitle}
+            {"I'm a full stack developer"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
